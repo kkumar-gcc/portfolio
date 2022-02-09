0b812dd6f13ebe91aab2b7e7b0641dbc503a7de8
kkumar-gcc <krishkumar9352@gmail.com>
2022-02-09 20:47:50 +0530

    contact page updated

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -4,28 +4,42 @@ import "./contact.css";
 function Contact() {
   return (
     <>
-      <div className="contact form-group">
-        <div className="contact-form-wrapper">
-          <label htmlFor="">Your Name</label>
-          <input type="text"/>
+      <div className="contact-container form-group">
+        <div className='contact-title'><h1>Contact Me</h1></div>
+        <div className='contact-content'>
+          <div className='contact-left-container'>
+            <p>Nevine Acotanza
+              Chief Operating Officer
+
+              I am available for freelance work. Connect with me via and call in to my account.Phone: +01234567890Email: admin@example.com
+              FIND WITH ME</p>
+          </div>
+          <div className='contact-right-container'>
+            <div className="contact-form-wrapper search">
+              <input type="text" className='search_input' placeholder='YOUR NAME' />
+              <div class="search_icon">
+                <ion-icon name="person-outline"></ion-icon>
+              </div>
+            </div>
+            <div className="contact-form-wrapper search">
+              <input type="email" className='search_input' placeholder='YOUR EMAIL' />
+              <div class="search_icon">
+                <ion-icon name="mail-outline"></ion-icon>
+              </div>
+            </div>
+            <div className="contact-form-wrapper search">
+              <input type="text" className='search_input' placeholder='YOUR SUBJECT' />
+              <div class="search_icon">
+                <ion-icon name="reader-outline"></ion-icon>
+              </div>
+            </div>
+
+            <div className="contact-form-wrapper">
+              
+              <textarea name="" id="" cols="30" rows="30" className='search_input' placeholder='YOUR MESSAGE....'></textarea>
+            </div>
+          </div>
         </div>
-        <div className="contact-form-wrapper">
-          <label htmlFor="">Phone Number</label>
-          <input type="text"/>
-        </div>
-        <div className="contact-form-wrapper">
-          <label htmlFor="">Email</label>
-          <input type="text"/>
-        </div>
-        <div className="contact-form-wrapper">
-          <label htmlFor="">Subject</label>
-          <input type="text"/>
-        </div>
-        <div className="contact-form-wrapper">
-          <label htmlFor="">Your message</label>
-          <textarea name="" id="" cols="30" rows="10"></textarea>
-        </div>
-      
 
       </div>
     </>
