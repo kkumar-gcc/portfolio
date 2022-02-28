e65a8d003c5d21797f4e4803683458bb90b15d4f
kkumar-gcc <krishkumar9352@gmail.com>
2022-02-28 12:58:32 +0530

    improved responsiveness

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -14,11 +14,12 @@ function Contact() {
             <ul className="contact_icons">
               <li>
                 {" "}
-                <a href="https://github.com/kkumar-gcc " style={{ color: "#3b5998" }}>
-                  <ion-icon
-                    name="logo-facebook"
-                    
-                  ></ion-icon> <span style={{ color: "#3b5998" }}>Like me on Facebook</span>
+                <a
+                  href="https://github.com/kkumar-gcc "
+                  style={{ color: "#3b5998" }}
+                >
+                  <ion-icon name="logo-facebook"></ion-icon>{" "}
+                  <span style={{ color: "#3b5998" }}>Like me on Facebook</span>
                 </a>
               </li>
               <li>
@@ -27,7 +28,8 @@ function Contact() {
                   <ion-icon
                     name="logo-twitter"
                     style={{ color: "#00acee" }}
-                  ></ion-icon> <span style={{ color: "#00acee" }}>Follow me on Twitter</span>
+                  ></ion-icon>{" "}
+                  <span style={{ color: "#00acee" }}>Follow me on Twitter</span>
                 </a>
               </li>
               <li>
@@ -36,7 +38,10 @@ function Contact() {
                   <ion-icon
                     name="logo-linkedin"
                     style={{ color: "#0e76a8" }}
-                  ></ion-icon> <span style={{ color: "#0e76a8" }}>Connect with me on Linkedin</span>
+                  ></ion-icon>{" "}
+                  <span style={{ color: "#0e76a8" }}>
+                    Connect with me on Linkedin
+                  </span>
                 </a>
               </li>
               <li>
@@ -45,7 +50,10 @@ function Contact() {
                   <ion-icon
                     name="logo-github"
                     style={{ color: "#AD5C51" }}
-                  ></ion-icon> <span  style={{ color: "#AD5C51" }}>See my Github profile</span>
+                  ></ion-icon>{" "}
+                  <span style={{ color: "#AD5C51" }}>
+                    See my Github profile
+                  </span>
                 </a>
               </li>
             </ul>
@@ -53,47 +61,57 @@ function Contact() {
 
           <div className="contact-right-container">
             <h5>* Send me a Email</h5>
-            <div className="contact-form-wrapper search">
-              <input
-                type="text"
-                className="form_input"
-                placeholder="YOUR NAME"
-              />
-              <div className="form_icon">
-                <ion-icon name="person-outline"></ion-icon>
+            <form>
+              <div className="contact-form-wrapper search">
+                <input
+                  type="text"
+                  className="form_input"
+                  placeholder="YOUR NAME"
+                />
+                <div className="form_icon">
+                  <ion-icon name="person-outline"></ion-icon>
+                </div>
               </div>
-            </div>
-            <div className="contact-form-wrapper search">
-              <input
-                type="email"
-                className="form_input"
-                placeholder="YOUR EMAIL"
-              />
-              <div className="form_icon">
-                <ion-icon name="mail-outline"></ion-icon>
+              <div className="contact-form-wrapper search">
+                <input
+                  type="email"
+                  className="form_input"
+                  placeholder="YOUR EMAIL"
+                />
+                <div className="form_icon">
+                  <ion-icon name="mail-outline"></ion-icon>
+                </div>
               </div>
-            </div>
-            <div className="contact-form-wrapper search">
-              <input
-                type="text"
-                className="form_input"
-                placeholder="YOUR SUBJECT"
-              />
-              <div className="form_icon">
-                <ion-icon name="reader-outline"></ion-icon>
+              <div className="contact-form-wrapper search">
+                <input
+                  type="text"
+                  className="form_input"
+                  placeholder="YOUR SUBJECT"
+                />
+                <div className="form_icon">
+                  <ion-icon name="reader-outline"></ion-icon>
+                </div>
               </div>
-            </div>
 
-            <div className="contact-form-wrapper">
-              <textarea
-                name=""
-                id=""
-                cols="30"
-                rows="30"
-                className="form_input_area"
-                placeholder="YOUR MESSAGE...."
-              ></textarea>
-            </div>
+              <div className="contact-form-wrapper">
+                <textarea
+                  name=""
+                  id=""
+                  cols="30"
+                  rows="30"
+                  className="form_input_area"
+                  placeholder="YOUR MESSAGE...."
+                ></textarea>
+              </div>
+              <div className="contact-form-wrapper submit-form">
+                <input
+                  type="submit"
+                  className="form_input_submit"
+                  value={"Submit"}
+                />
+               
+              </div>
+            </form>
           </div>
         </div>
       </div>
