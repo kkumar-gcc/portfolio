dd70fa96cdd262a0fce2ce73e83d1215fdc3f976
kkumar-gcc <84431594+kkumar-gcc@users.noreply.github.com>
2022-03-22 16:29:08 +0530

    Update home.jsx

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,13 +1,19 @@
 import "./home.css";
 import Typewriter from "typewriter-effect";
-const Home=()=> {
+const Home = () => {
   return (
     <>
-      <div className="home-container">
+     <div>
+        <div className="title-hide" id="hideDiv">
+          <div className="px-2">
+             <span className="home-subtitle">Welcome to my portfolio</span>
+          </div>
+        </div>
+        <div className="home-container">
         <div className="home-left-container">
-          <span className="home-subtitle">Welcome to my portfolio</span>
+         
           <h1>
-            Hi, I'm <span className="home-title-name">Krishan Kumar</span>
+            <span className="home-title-name">Krishan Kumar</span>
           </h1>
           <span className="article"> a </span>
           <Typewriter
@@ -17,24 +23,24 @@ const Home=()=> {
               loop: true,
             }}
           />
-          <p className="desc">
+          {/* <p className="desc">
             I'm a Tunisian based web designer & front‑end developer focused on
             crafting clean & user‑friendly experiences, I am passionate about
             building excellent software that improves the lives of those around
             me.
-          </p>
+          </p> */}
         </div>
         <div className="home-right-container">
           <div className="image-container">
-            
             <div className="image-inner-container">
               <img src={"https://picsum.photos/280/340"} alt="" />
             </div>
           </div>
         </div>
       </div>
+      </div>
     </>
   );
-}
+};
 
 export default Home;
